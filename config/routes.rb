6c97cd843a3005269bda73f55fee5c5276a6f449
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-10-01 20:30:14 +0000

    Added to photos index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index"})   
   get("/users/:path_username", { :controller => "users", :action => "show"})
 
+  get("/photos", { :controller => "photos", :action => "index"})
 
 end

8204a471c959aab2806eb3493be3da2ddbe1f407
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-10-01 20:11:04 +0000

    Added most user details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,17 @@ class UsersController < ApplicationController
 
     @list_of_users = matching_users.order ({ :username => :asc })
 
-    
 
-    render ({ :template => "user_templates/index"})
+
+    render ({ :template => "user_templates/index" })
   end
 
+  def show
+    # Parameters: {"path_username"=>"anisa"}
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({ :username => url_username })
+    @the_user = matching_usernames.first
+
+    render ({ :template => "user_templates/show" })
+  end
 end

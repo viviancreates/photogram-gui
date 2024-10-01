5c8d6acf18d063bf73e9d634c84594361f75548c
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-10-01 23:19:46 +0000

    save

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -52,8 +52,27 @@ redirect_to("/photos/" + a_new_photo.id.to_s)
 end
 
 def update
+  #Parameters: {"query_image"=>"a", "query_caption"=>"aakjkj", "modify_id"=>"952"}
 
-  render({ :template => "photo_templates/update"})
+  the_id = params.fetch("modify_id")
+
+  matching_photos = Photo.where ({ :id => the_id })
+
+  the_photo = matching_photos.at(0)
+
+  input_image = params.fetch("query_image")
+  input_caption = params.fetch("query_caption")
+
+  the_photo.image = input_image
+  the_photo.caption = input_caption
+
+  the_photo.save
+
+  #render({ :template => "photo_templates/update"})
+
+  next_url = "/photos/" + the_photo.id.to_s
+
+  redirect_to(next_url)
 end
 
 end
